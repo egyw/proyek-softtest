de0cd76c3011217d6f5d753b2fb192eaf4b973d3
Hans Christian <152836345+hansckk@users.noreply.github.com>
2025-12-24 13:33:51 +0700

    update variable

--- src/main/java/com/proyek_softtest/pages/NewsPage.java
+++ src/main/java/com/proyek_softtest/pages/NewsPage.java
@@ -9,13 +9,28 @@ import com.proyek_softtest.utils.Delay;
 
 public class NewsPage extends BasePage {
     private By homeBreadCrumbLink = By.linkText("safe.openproject.com");
+    private By openProjectLink = By.linkText("safe.openproject.com");
+    private By demoProjectLink = By.linkText("Demo project");
+    private By clickTestSyntaxHighlightingLink = By.linkText("Test syntax highlighting");
+    private By demoAdminLink = By.linkText("Demo admin");
+    private By actualitesPageLink = By.linkText(
+            "Actualités - [Acteurspublics.fr] Réforme de la haute fonction publique : le cas de l’IGF interroge toujours");
+    private By scrumProjectLink = By.linkText("Scrum project");
+    private By welcomeToYourScrumDemoProjectLink = By.linkText("Welcome to your Scrum demo project");
+    private By welcomeToYourDemoProjectLink = By.linkText("Welcome to your demo project");
+    private By pagination50 = By.linkText("50");
+    private By pagination100 = By.linkText("100");
+    private By pagination200 = By.linkText("200");
+    private By demoProjectLinkSecond = By.xpath("(//a[text()='Demo project'])[2]");
+    private By demoProjectLinkThird = By.xpath("(//a[text()='Demo project'])[3]");
+    private By demoAdminLinkSecond = By.xpath("(//a[text()='Demo admin'])[2]");
 
     public NewsPage(WebDriver driver) {
         super(driver);
     }
 
     // ╔════════════════════════════════════════════════════════╗
-    // ║ NEWS PAGE ACTIONS                                      ║
+    // ║ NEWS PAGE ACTIONS ║
     // ╚════════════════════════════════════════════════════════╝
 
     public NewsPage clickHomeBreadCrumbLink() {
@@ -26,7 +41,7 @@ public class NewsPage extends BasePage {
     }
 
     public NewsPage clickOpenProjectLink() {
-        By openProjectLink = By.linkText("safe.openproject.com");
+
         wait.until(ExpectedConditions.elementToBeClickable(openProjectLink)).click();
         wait.until(ExpectedConditions.urlToBe("https://safe.openproject.com/"));
         Delay.waitDefault();
@@ -34,7 +49,7 @@ public class NewsPage extends BasePage {
     }
 
     public NewsPage clickDemoProjectLink() {
-        By demoProjectLink = By.linkText("Demo project");
+
         wait.until(ExpectedConditions.elementToBeClickable(demoProjectLink)).click();
         wait.until(ExpectedConditions.urlToBe("https://safe.openproject.com/projects/demo-project"));
         Delay.waitDefault();
@@ -42,7 +57,7 @@ public class NewsPage extends BasePage {
     }
 
     public NewsPage clickTestSyntaxHighlightingLink() {
-        By clickTestSyntaxHighlightingLink = By.linkText("Test syntax highlighting");
+
         wait.until(ExpectedConditions.elementToBeClickable(clickTestSyntaxHighlightingLink)).click();
         wait.until(ExpectedConditions.urlToBe("https://safe.openproject.com/news/4-test-syntax-highlighting"));
         Delay.waitDefault();
@@ -50,7 +65,7 @@ public class NewsPage extends BasePage {
     }
 
     public NewsPage clickDemoAdminLink() {
-        By demoAdminLink = By.linkText("Demo admin");
+
         wait.until(ExpectedConditions.elementToBeClickable(demoAdminLink)).click();
         wait.until(ExpectedConditions.urlToBe("https://safe.openproject.com/users/10"));
         Delay.waitDefault();
@@ -58,8 +73,7 @@ public class NewsPage extends BasePage {
     }
 
     public NewsPage clickActualitesPageLink() {
-        By actualitesPageLink = By.linkText(
-                "Actualités - [Acteurspublics.fr] Réforme de la haute fonction publique : le cas de l’IGF interroge toujours");
+
         wait.until(ExpectedConditions.elementToBeClickable(actualitesPageLink)).click();
         wait.until(ExpectedConditions.urlToBe(
                 "https://safe.openproject.com/news/3-actualites-acteurspublics-fr-reforme-de-la-haute-fonction-publique-le-cas-de-l-igf-interroge-toujours"));
@@ -68,7 +82,7 @@ public class NewsPage extends BasePage {
     }
 
     public NewsPage clickScrumProjectLink() {
-        By scrumProjectLink = By.linkText("Scrum project");
+
         wait.until(ExpectedConditions.elementToBeClickable(scrumProjectLink)).click();
         wait.until(ExpectedConditions.urlToBe("https://safe.openproject.com/projects/your-scrum-project"));
         Delay.waitDefault();
@@ -76,7 +90,7 @@ public class NewsPage extends BasePage {
     }
 
     public NewsPage clickWelcomeToYourScrumDemoProjectLink() {
-        By welcomeToYourScrumDemoProjectLink = By.linkText("Welcome to your Scrum demo project");
+
         wait.until(ExpectedConditions.elementToBeClickable(welcomeToYourScrumDemoProjectLink)).click();
         wait.until(
                 ExpectedConditions.urlToBe("https://safe.openproject.com/news/2-welcome-to-your-scrum-demo-project"));
@@ -85,7 +99,7 @@ public class NewsPage extends BasePage {
     }
 
     public NewsPage clickWelcomeToYourDemoProjectLink() {
-        By welcomeToYourDemoProjectLink = By.linkText("Welcome to your demo project");
+
         wait.until(ExpectedConditions.elementToBeClickable(welcomeToYourDemoProjectLink)).click();
         wait.until(ExpectedConditions.urlToBe("https://safe.openproject.com/news/1-welcome-to-your-demo-project"));
         Delay.waitDefault();
@@ -93,28 +107,28 @@ public class NewsPage extends BasePage {
     }
 
     public NewsPage clickPagination50() {
-        By pagination50 = By.linkText("50");
+
         wait.until(ExpectedConditions.elementToBeClickable(pagination50)).click();
         Delay.waitDefault();
         return this;
     }
 
     public NewsPage clickPagination100() {
-        By pagination100 = By.linkText("100");
+
         wait.until(ExpectedConditions.elementToBeClickable(pagination100)).click();
         Delay.waitDefault();
         return this;
     }
 
     public NewsPage clickPagination200() {
-        By pagination200 = By.linkText("200");
+
         wait.until(ExpectedConditions.elementToBeClickable(pagination200)).click();
         Delay.waitDefault();
         return this;
     }
 
     public NewsPage clickDemoProjectLinkSecond() {
-        By demoProjectLinkSecond = By.xpath("(//a[text()='Demo project'])[2]");
+
         wait.until(ExpectedConditions.elementToBeClickable(demoProjectLinkSecond)).click();
         wait.until(ExpectedConditions.urlToBe("https://safe.openproject.com/projects/demo-project"));
         Delay.waitDefault();
@@ -122,7 +136,7 @@ public class NewsPage extends BasePage {
     }
 
     public NewsPage clickDemoPorjectLinkThird() {
-        By demoProjectLinkThird = By.xpath("(//a[text()='Demo project'])[3]");
+
         wait.until(ExpectedConditions.elementToBeClickable(demoProjectLinkThird)).click();
         wait.until(ExpectedConditions.urlToBe("https://safe.openproject.com/projects/demo-project"));
         Delay.waitDefault();
@@ -130,7 +144,7 @@ public class NewsPage extends BasePage {
     }
 
     public NewsPage clickDemoAdminLinkSecond() {
-        By demoAdminLinkSecond = By.xpath("(//a[text()='Demo admin'])[2]");
+
         wait.until(ExpectedConditions.elementToBeClickable(demoAdminLinkSecond)).click();
         wait.until(ExpectedConditions.urlToBe("https://safe.openproject.com/users/10"));
         Delay.waitDefault();
